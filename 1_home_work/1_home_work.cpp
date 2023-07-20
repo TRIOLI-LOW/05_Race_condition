e2b391f09f18983000955c17750aacf60e89147a
TRIOLI LOW <trioli.low@gmail.com>
2023-07-20 22:32:26 +0300

    init

---
+++ 1_home_work/1_home_work.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <thread>
+#include <Windows.h>
+#include <chrono>
+#include <mutex>
+#include <condition_variable>
+
+std::mutex m;
+std::condition_variable cv;
+bool StopFlag = false;
+std::atomic<int> clientCount = 0;  // Счетчик клиентов
+std::atomic<int> maxClients = 10;  // Максимальное количество клиентов
+
+// Функция, имитирующая поток клиента
+void clientThread()
+{
+    while (!StopFlag)
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+
+        if (clientCount < maxClients)
+        {
+            std::unique_lock<std::mutex> lock(m);
+            ++clientCount;
+            std::cout << "Пришел клиент. Всего клиентов: " << clientCount << std::endl;
+            cv.notify_all();
+        }
+    }
+}
+
+// Функция, имитирующая поток операциониста
+void operatorThread()
+{
+    while (!StopFlag)
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        
+        std::unique_lock<std::mutex> lock(m);
+        cv.wait(lock, [] {return clientCount > 0 || StopFlag; });
+
+        if (StopFlag)
+            break;
+
+         --clientCount;
+         std::cout << "Обслужен клиент. Осталось клиентов: " << clientCount << std::endl;
+            
+
+        
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::thread client(clientThread);
+    std::thread oper(operatorThread);
+
+
+  
+   // std::this_thread::sleep_for(std::chrono::seconds(11));
+   // StopFlag = true;
+    client.join();
+    oper.join();
+
+    return 0;
+}
---
+++ 3_home_work/3_home_work.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+#include <mutex>
+#include <thread>
+
+
+template <typename T>
+class Data {
+private: 
+	T scalarData;
+public: 
+
+	Data(T scalarData_) : scalarData(scalarData_) {};
+
+	T getData() const {
+		return scalarData;
+	}
+	void setData(T newData) {
+		scalarData = newData;
+	}
+
+	std::mutex m;
+};
+
+
+template <typename T>
+void swap_lock(Data<T>& data1, Data<T>& data2) {
+	std::lock(data1.m, data2.m);
+	std::lock_guard<std::mutex> lock1(data1.m, std::adopt_lock);
+	std::lock_guard<std::mutex> lock2(data2.m, std::adopt_lock);
+
+	T temp = data1.getData();
+	data1.setData(data2.getData());
+	data2.setData(temp);
+ }
+template <typename T>
+void swap_scoped(Data<T>& data1, Data<T>& data2) {
+	std::scoped_lock lock(data1.m, data2.m);
+
+	T temp = data1.getData();
+	data1.setData(data2.getData());
+	data2.setData(temp);
+}
+
+template <typename T>
+void swap_unique(Data<T>& data1, Data<T>& data2) {
+	std::unique_lock<std::mutex> lock1(data1.m, std::defer_lock);
+	std::unique_lock<std::mutex> lock2(data2.m, std::defer_lock);
+	std::lock(lock1, lock2);
+
+
+	T temp = data1.getData();
+	data1.setData(data2.getData());
+	data2.setData(temp);
+}
+
+
+
+	 int main() {
+	 Data<int> data1(15);
+	 Data<int> data2(54);
+
+	 std::cout << data1.getData() << std::endl;
+	 std::cout << data2.getData() << std::endl;
+	 std::cout << std::endl;
+
+	 swap_lock(data1, data2);
+
+	 std::cout << data1.getData() << std::endl;
+	 std::cout << data2.getData() << std::endl;
+	 std::cout << std::endl;
+
+	 swap_scoped(data1, data2);
+
+	 std::cout << data1.getData() << std::endl;
+	 std::cout << data2.getData() << std::endl;
+	 std::cout << std::endl;
+
+	 swap_unique(data1, data2);
+
+	 std::cout << data1.getData() << std::endl;
+	 std::cout << data2.getData() << std::endl;
+ }
